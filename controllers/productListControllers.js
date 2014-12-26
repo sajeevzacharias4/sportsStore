43f40de655719f500bce64db217be55a26f8c1cd
sajeevzacharias4 <sajeev.zacharias4@gmail.com>
2014-12-26 11:42:22 +0000

    second commit

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -2,7 +2,7 @@ angular.module("sportsStore")
 	.constant("productListActiveClass", "btn-primary")
 	.constant("productListPageCount", 3)
 	.controller("productListCtrl", function ($scope, $filter,
-productListActiveClass, productListPageCount) {
+productListActiveClass, productListPageCount, cart) {
 		var selectedCategory = null;
 		$scope.selectedPage = 1;
 		$scope.pageSize = productListPageCount;
@@ -28,4 +28,8 @@ productListActiveClass, productListPageCount) {
 		$scope.getPageClass = function (page) {
 			return $scope.selectedPage == page ? productListActiveClass : "";
 		}
+        
+        $scope.addProductToCart = function (product) {
+            cart.addProduct(product.objectId, product.name, product.price);
+        }
 });
--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,14 +1,16 @@
 angular.module('sportsStore')
-.controller('sportsStoreCtrl', function($scope){
-	$scope.data={
-		products:[
-			{name:'Product #1', description:'A product', 
-				category:'Category #1', price:100 },
-			{name:'Product #2', description:'A product', 
-				category:'Category #1',price:110 },
-			{name:'Product #3', description:'A product', 
-				category:'Category #2', price:210 },
-			{name:'Product #4', description:'A product', 
-				category:'Category #3', price:202 }]
-};
-});			
+	.constant("dataUrl", "https://api.parse.com/1/classes/Products")
+	.run(function($http){
+		$http.defaults.headers.common["X-Parse-Application-Id"]="AMeeQgh6azzidQhfjrjq56hscgcidUfSKxatMi90";
+		$http.defaults.headers.common["X-Parse-REST-API-Key"]="JuCbncI6PGy53B5UCvMZuUuGnxBxbeQ7HgjLToH9";
+	})
+	.controller('sportsStoreCtrl', function($scope, $http, dataUrl){
+	$scope.data={};
+		$http.get(dataUrl)
+			.success(function (data) {
+				$scope.data.products = data.results;
+			})
+			.error(function (error) {
+				$scope.data.error = error;
+});
+});
